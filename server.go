5dc080de530b569832d351556b97d2b02e579c91
eric-lab-star <cyon256@icloud.com>
2022-11-02 01:02:00 +0900

    hot reload with air

--- server.go
+++ server.go
@@ -6,13 +6,13 @@ import (
 )
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello world</h1>")
+	fmt.Fprint(w, "<h1>Hi  is watching</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handleHome)
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
 		fmt.Println("err", err)
 	}
