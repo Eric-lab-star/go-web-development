245f5d966aeefa64bd8a2fe43a3d71fbc0ec7a87
eric-lab-star <cyon256@icloud.com>
2022-11-03 20:06:59 +0900

    learn header

--- server.go
+++ server.go
@@ -6,12 +6,15 @@ import (
 )
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hi  is watching</h1>")
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	w.Header().Add("transfer-encoding", "chunked")
+	w.Header().Add("trailer", "transfer-encoding")
+	w.Write([]byte("heelo"))
+	fmt.Fprint(w, "<h1>Hi air is watching</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handleHome)
-
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
 		fmt.Println("err", err)
