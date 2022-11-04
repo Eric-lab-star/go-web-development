001b3ca32c4b2451c2154d4baea678e40172c576
eric-lab-star <cyon256@icloud.com>
2022-11-04 09:28:59 +0900

    simple contact page

--- server.go
+++ server.go
@@ -5,16 +5,18 @@ import (
 	"net/http"
 )
 
-func handleHome(w http.ResponseWriter, r *http.Request) {
+func homHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html; charset=utf-8")
-	w.Header().Add("transfer-encoding", "chunked")
-	w.Header().Add("trailer", "transfer-encoding")
-	w.Write([]byte("heelo"))
-	fmt.Fprint(w, "<h1>Hi air is watching</h1>")
+	fmt.Fprint(w, "<a href=\"/contact\"><h1>contact page</h1></a>")
+}
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:cyon256@icloud.com\">cyon256@icloud.com</a></p>")
 }
 
 func main() {
-	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/", homHandler)
+	http.HandleFunc("/contact", contactHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
 		fmt.Println("err", err)
