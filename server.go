88d6008155713a7fab842cfa6a2dc1c4baa14287
eric-lab-star <cyon256@icloud.com>
2022-11-04 18:45:01 +0900

    faq pag

--- server.go
+++ server.go
@@ -14,23 +14,30 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:cyon256@icloud.com\">cyon256@icloud.com</a></p>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>faq page</h1>")
+}
+
 type Router struct{}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.NotFound(w, r)
 	}
 }
 
 func main() {
+	var router Router
+	fmt.Println("Starting server on http://localhost:8080")
+	http.ListenAndServe("localhost:8080", router)
 
-	err := http.ListenAndServe("localhost:8080", http.HandlerFunc(homeHandler))
-	if err != nil {
-		fmt.Println("err", err)
-	}
 }
